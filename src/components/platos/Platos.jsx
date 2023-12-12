12b6da7ed933657a991525522e7186a4f10faa05
eduardocl22 <71535251+eduardocl22@users.noreply.github.com>
2023-12-11 23:22:03 -0400

    Update Platos.jsx

--- src/components/platos/Platos.jsx
+++ src/components/platos/Platos.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
-import Titulo from '../layouts/Titulo'
-=======
 import Titulo from '../layouts/titulo'
->>>>>>> d5adabf346dd41201354ce9e7b2f7285c431cbf2
 import ListaPlatos from './ListaPlatos'
 import Footer from '../layouts/Footer'
 
@@ -19,3 +15,4 @@ export default function Platos() {
     </>
   )
 }
+
